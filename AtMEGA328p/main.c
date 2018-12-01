e401b983e17482ae7d3d2bdd04fad595fc649629
raineym5 <raineym5@students.rowan.edu>
2018-12-01 15:53:57 -0500

    added comments

--- AtMEGA328p/main.c
+++ AtMEGA328p/main.c
@@ -10,25 +10,25 @@
 void USART_Init( unsigned int ubrr){
 	UBRR0H = (unsigned char)(ubrr>>8);
 	UBRR0L = (unsigned char)ubrr;//setup usart bits
-	UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0);//tx rx en
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0);//tx rx enable, RX interrupt enable
 }
 void Timer_Init(void){
-	TCCR0A = (1<<COM0A1)|(1<<WGM01)|(1<<WGM00);//fast pwm
-	TCCR0B = (1<<CS01);//3.9 KHz PWM
-	OCR0A = 0;//Timer0 PWM value
+	TCCR0A = (1<<COM0A1)|(1<<WGM01)|(1<<WGM00);//fast pwm on timer0
+	TCCR0B = (1<<CS01);//3.9 KHz overflow on timer 0
+	OCR0A = 0;//Timer0 PWM value init
 
 	TCCR1B = (1<<CS11)|(1<<WGM12); //timer 1 clockdiv 
-	OCR1AH = (1<<6)|(1<<1);
-	OCR1AL = 0;
+	OCR1AH = (1<<6)|(1<<1);//timer 1 overflow rate high byte
+	OCR1AL = 0;//low byte
 	TIMSK1 = (1<<OCIE1A);//timer 1 interrupt enable
 	
 }
 void ADC_init(void){
-	ADMUX = (1<<MUX0)|(1<<ADLAR);//(1<<ADCn);//xn for adc pin
-	ADCSRA = (1<<ADEN)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADATE);//125 KHz, |(1<<ADIE)
-	ADCSRB = (1<<ADTS2)|(1<<ADTS1);
+	ADMUX = (1<<MUX0)|(1<<ADLAR);//ADC pin 24
+	ADCSRA = (1<<ADEN)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADATE);//125 KHz ADC
+	ADCSRB = (1<<ADTS2)|(1<<ADTS1);//manual interrupt
 }
-uint8_t REF = 30;//pretty good: 20 1 4
+uint8_t REF = 30;
 const uint8_t 	kp 	= 128,
 				ki 	= 1,
 				kd 	= 40;
@@ -39,19 +39,20 @@ int16_t p,i=0,d;//Several methods for tuning the PID loop exist. The choice of m
 //P		0.5*Kc	/		/
 //PD	0.5*Kc	/		0.12Pd
 //PI	0.45Kc	0.85Pc	/
-//PID	0.65Kc	0.5Pc	0.12Pd		
+//PID	0.65Kc	0.5Pc	0.12Pd	
+//It was discovered that these dit not work, and these values had to be found out manually.	
 uint8_t ADCtoPWM(int8_t temp, int8_t error[2]){
-	uint16_t total;
+	uint16_t total;//made 16 bit so there's no overflows
 	
-	p =  kp*error[0];
+	p =  kp*error[0];//P term
 	if(p >= 255)
-		p = 255;
+		p = 255;//no overflows or underflows
 	if(p < 0)
 		p=0;
 		
-	i = i + ki*error[0];
+	i = i + ki*error[0];//I term
 	if(i >= 255)
-		i = 255;
+		i = 255;//no overflows or underflows
 	else if(i < 0)
 		i=0;
 		
@@ -59,16 +60,16 @@ uint8_t ADCtoPWM(int8_t temp, int8_t error[2]){
 	if(p+i == 0)
 		d=0;
 	total = p+i+d;
-	if(total >= 255)
+	if(total >= 255)//no overflows
 		return 255;
-	return (uint8_t)total;//
+	return (uint8_t)total;//mask top 8 bits
 }
 
 
 uint8_t ADCtoTEMP(uint8_t ADCval){
 	if(ADCval < 189)
-		return (uint8_t)(((uint16_t)105*ADCval)/256 - 17);//let the comiler decide on whether to shift, swap nibbles, or something crazy, etc.
-	return (uint8_t)(((uint16_t)225*ADCval)/256 - 106);
+		return (uint8_t)(((uint16_t)105*ADCval)/256 - 17);//let the comiler decide on whether to shift, swap nibbles, or something crazy, etc. 105/256 was picked because 0.41 is needed, and division should be divisible by 
+	return (uint8_t)(((uint16_t)225*ADCval)/256 - 106);//same here
 }
 ISR(TIMER1_COMPA_vect){
 //used to wake up CPU
@@ -79,10 +80,10 @@ ISR(ADC_vect){
 
 }
 void USART_Transmit( uint8_t data ){
-	while ( !( UCSR0A & (1<<UDRE0)) );
+	while ( !( UCSR0A & (1<<UDRE0)) );//wait until buffer is empty
 	UDR0 = data;
 }
-void sendint(uint8_t temp){
+void sendint(uint8_t temp){//converts binary to ascii
 	uint8_t tempbytes[] = {0,0,0};
 	while(temp >= 100){
 		tempbytes[2]++;
@@ -94,19 +95,19 @@ void sendint(uint8_t temp){
 		tempbytes[0]++;
 		temp-=1;
 	}
-	USART_Transmit(tempbytes[2]+48);
+	USART_Transmit(tempbytes[2]+48);//transmit the result over UART
 	USART_Transmit(tempbytes[1]+48);
 	USART_Transmit(tempbytes[0]+48);
 
 }
-uint8_t rec[] = {0,0,0,0};
+uint8_t rec[] = {0,0,0,0};//received UART bits
 uint8_t ctr=0;
 ISR(USART_RX_vect){
 	uint8_t temp = UDR0;
 	rec[ctr] = temp;
 	if(ctr < 4)
 		ctr++;
-	if(temp == '\r'){
+	if(temp == '\r'){//if enter, set temp
 		if(ctr == 3)
 			REF = 10*(rec[0]-112)+(rec[1]-112);
 		rec[0]=0;
@@ -114,7 +115,7 @@ ISR(USART_RX_vect){
 		rec[2]=0;
 		ctr = 0;
 	}	
-	else if(temp == '\b'){
+	else if(temp == '\b'){//if backspace, remove chars
 		rec[0]=0;
 		rec[1]=0;
 		rec[2]=0;
@@ -126,36 +127,35 @@ ISR(USART_RX_vect){
 int main(void){
 
 	DDRB = 0xFF;
-	DDRC = 0xF0;
-	DDRD = 0xF0;
-	USART_Init(MYUBRR);
-	Timer_Init();
-	ADC_init();
+	DDRC = 0xFD;
+	DDRD = 0xFF;//set all to output except used bits
+	USART_Init(MYUBRR);//init uart
+	Timer_Init();//init timer
+	ADC_init();//init ADC
 	uint8_t temp=0,ADCval = 0;
 	int8_t error[] = {0,0};
 	while(1){
-		ADCSRA |= (1<<ADSC);
-		//PORTD |= (1<<PD2);
-		asm("SEI");
-		SMCR = (1<<SE);
-		asm("sleep");
+		ADCSRA |= (1<<ADSC);//enable conversion
+		asm("SEI");//set up interrupts
+		SMCR = (1<<SE);//sleep enable
+		asm("sleep");//sleep
 		
-		ADCval = ADCH;
-		temp = ADCtoTEMP(ADCval);
-		error[0] = temp - REF;
-		OCR0A = ADCtoPWM(temp,error);
+		ADCval = ADCH;//read adc
+		temp = ADCtoTEMP(ADCval);//convert to temperature
+		error[0] = temp - REF;//save error
+		OCR0A = ADCtoPWM(temp,error);//convert to PWM value using PID
 		
 		USART_Transmit('T');
 		USART_Transmit(':');
-		sendint(temp);
+		sendint(temp);//send temp
 		USART_Transmit(0x09);
 		USART_Transmit('R');
 		USART_Transmit(':');
-		sendint(REF);
+		sendint(REF);//send ref
 		error[1] = error[0];
 		//sendint(OCR0A);//pwm value
 		USART_Transmit('\r');
-		USART_Transmit('\n');
+		USART_Transmit('\n');//return, new line
 	}
 	return 0;
 }
